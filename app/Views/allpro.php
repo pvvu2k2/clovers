b42b1d6ead8534f5914593e4afa887b42028d5aa
pvvu2k2 <vupvps38989@fpt.edu.vn>
2024-06-07 08:43:58 +0700

    chỉnh sữa hiển thị tất cả các sản phẩm hiển thị theo danh mục chỉnh sữa nếu nó là bestdeal thì hiển thị giá giảm và ngược lại thì không

--- app/Views/allpro.php
+++ app/Views/allpro.php
@@ -61,28 +61,32 @@
 
             // var_dump($allidpro['id'], $is_bestseller, $is_bestdeal);
 
-            extract($allidpro);
-            $priceSale = $price * 0.9;
-
             if ($is_bestseller) {
                 $html = '<p class="text-tick">Most Popular</p>';
             } else if ($is_bestdeal) {
                 $html = '<p class="text-tick">Best Deals</p>';
-                $html .= '<p>' . $priceSale . '</p>';
             } else {
                 $html = '';
             }
 
+            extract($allidpro);
+            $priceSale = $price * 0.9;
 
         ?>
 
             <section>
                 <a href="index.php?page=detail&id=<?= $id ?>"><img src="./public/uploads/<?= $img ?>"></a>
-
+                <?php echo $html; ?>
                 <div>
                     <p class="name"><?= $name ?></p>
-                    <p class="price"><span>$</span><?= $price ?></p>
-                    <?php echo $html; ?>
+                    <?php
+                    if ($is_bestdeal) { ?>
+                        <p class="price"><span style="margin-right:20px; text-decoration: line-through;">$<?= $price ?></span>
+                            $<?= $priceSale ?></p>
+                    <?php } else { ?>
+                        <p class="price">$<?= $price ?></p>
+                    <?php } ?>
+
                 </div>
                 <a href="#" class="btn_addToCart">Add to Cart</a>
             </section>
